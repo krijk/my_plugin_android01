ae84f80e5b33d4eb962e7df3d813cc8e8aab2cfe
krijk <kenvanrijk@gmail.com>
2022-07-20 17:21:15 +0900

    Introduced InfoAndroid

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
-import 'package:my_plugin_android01/my_plugin_android01.dart';
+import 'info_android.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,61 +14,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = '';
-  String _releaseVersion = '';
-  String _releaseName = '';
-  String _apiVersion = '';
-  final _myPluginAndroid01Plugin = MyPluginAndroid01();
+
+  late InfoAndroid infoAndroid;
 
   @override
   void initState() {
+    infoAndroid = InfoAndroid();
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion = await _myPluginAndroid01Plugin.getPlatformVersion() ?? 'PlatformVersion: Unknown';
-    } on PlatformException {
-      platformVersion = 'PlatformVersion: Failed to get';
-    }
-
-    String releaseVersion;
-    try {
-      releaseVersion = await _myPluginAndroid01Plugin.getReleaseVersion() ?? 'ReleaseVersion: Unknown';
-    } on PlatformException {
-      releaseVersion = 'ReleaseVersion: Failed to get';
-    }
-
-    String releaseName;
-    try {
-      releaseName = await _myPluginAndroid01Plugin.getReleaseName() ?? 'releaseName: Unknown';
-    } on PlatformException {
-      releaseName = 'releaseName: Failed to get';
-    }
-
-    String apiVersion;
-    try {
-      apiVersion = await _myPluginAndroid01Plugin.getAPIVersion() ?? 'APIVersion: Unknown';
-    } on PlatformException {
-      apiVersion = 'SDKVersion: Failed to get';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-      _releaseVersion = releaseVersion;
-      _releaseName = releaseName;
-      _apiVersion = apiVersion;
-    });
   }
 
   @override
@@ -86,10 +36,7 @@ class _MyAppState extends State<MyApp> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text('Running on:'),
-              Text(_platformVersion),
-              Text(_releaseVersion),
-              Text(_releaseName),
-              Text(_apiVersion),
+              Text(infoAndroid.toString()),
             ],
           ),
         ),
