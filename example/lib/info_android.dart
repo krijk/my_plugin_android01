4f9801930af93d592835b805ef20f3cdca98791b
krijk <kenvanrijk@gmail.com>
2022-07-21 18:09:12 +0900

    solved state update problems

--- example/lib/info_android.dart
+++ example/lib/info_android.dart
@@ -2,69 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:my_plugin_android01/my_plugin_android01.dart';
 
+const String _strUnknown = 'Unknown';
+const String _strFailed = 'Failed';
+
+/// Android Information
+// ignore: must_be_immutable
 class InfoAndroid extends StatefulWidget {
-  const InfoAndroid({Key? key}) : super(key: key);
+  /// Release version in numbers
+  String releaseVersion = _strUnknown;
+
+  /// Release version in strings
+  String releaseName = '';
+
+  /// API/SDK version in strings
+  String apiVersion = _strUnknown;
+
+  /// API/SDK version in numbers
+  int apiInt = 0;
+
+  ///
+  InfoAndroid({Key? key}) : super(key: key);
 
   @override
   State<InfoAndroid> createState() => InfoAndroidState();
 }
 
+///
 class InfoAndroidState extends State<InfoAndroid> {
   final MyPluginAndroid01 _myPluginAndroid01Plugin = MyPluginAndroid01();
 
-  static const String strUnknown = 'Unknown';
-  static const String strFailed = 'Failed';
-
-  String releaseVersion = '';
-  String releaseName = '';
-  String apiVersion = '';
-  int apiInt = -1;
-
-  InfoAndroidState() {
-    init();
-  }
-
+  /// should be called from e.g. 'WidgetsBinding.instance.addPostFrameCallback'
   Future<void> init() async {
     await getReleaseVersion();
     await getReleaseName();
     await getApiVersion();
+    await getApiInt();
     setState(() {});
   }
 
+  ///
   Future<void> getReleaseVersion() async {
+    String releaseVersion;
     try {
-      releaseVersion = await _myPluginAndroid01Plugin.getReleaseVersion() ?? strUnknown;
+      releaseVersion = await _myPluginAndroid01Plugin.getReleaseVersion() ?? _strUnknown;
     } on PlatformException {
-      releaseVersion = strFailed;
+      releaseVersion = _strFailed;
     }
+    widget.releaseVersion = releaseVersion;
   }
 
+  ///
   Future<void> getReleaseName() async {
+    String releaseName;
     try {
       releaseName = await _myPluginAndroid01Plugin.getReleaseName() ?? '';
     } on PlatformException {
-      releaseName = strFailed;
+      releaseName = _strFailed;
     }
+    widget.releaseName = releaseName;
   }
 
+  ///
   Future<void> getApiVersion() async {
+    String apiVersion;
     try {
-      apiVersion = await _myPluginAndroid01Plugin.getApiVersion() ?? strUnknown;
+      final int? api = await getApiInt();
+      if (api != null) {
+        apiVersion = '$api';
+      } else {
+        apiVersion = _strUnknown;
+      }
     } on PlatformException {
-      apiVersion = strFailed;
+      apiVersion = _strFailed;
     }
+    widget.apiVersion = apiVersion;
   }
 
-  Future<void> getApiInt() async {
-    try {
-      apiInt = await _myPluginAndroid01Plugin.getApiInt() ?? -1;
-    } on PlatformException {
-      apiInt = -2;
-    }
+  ///
+  Future<int?> getApiInt() async {
+    final int apiInt = await _myPluginAndroid01Plugin.getApiInt() ?? -1;
+    widget.apiInt = apiInt;
+    return apiInt;
   }
 
+  ///
   String get info {
-    return 'Android$releaseVersion API$apiVersion ($releaseName) : apiInt=$apiInt';
+    return 'Android${widget.releaseVersion} API${widget.apiVersion} (${widget.releaseName}) : apiInt=${widget.apiInt}';
   }
 
   @override
